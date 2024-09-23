99330cf7856e0cdd7c923944702b9742691d69a3
TKTek <TGJek@qq.com>
2024-09-23 23:47:43 +0800

    项目文件系统结构变动。

---
+++ src/数据结构/所有房间数据类.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TGZG.战雷革命房间服务器 {
+	public struct 所有房间数据类 {
+        public List<房间数据类> 房间列表;
+        [JsonIgnore]
+        public int 房间总数 => 房间列表.Count;
+        [JsonIgnore]
+        public 房间数据类 this[int index] => 房间列表[index];
+        [JsonIgnore]
+        public 房间数据类 this[string 房间名] => 房间列表.Find(x => x.房间名 == 房间名);
+        public bool 存在房间(string 房间名) => 房间列表.Exists(x => x.房间名 == 房间名);
+    }
+}
