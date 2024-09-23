1429602c39a9440d690d9da3d04880aa52c7f205
TKTek <TGJek@qq.com>
2024-09-23 20:15:56 +0800

    一些项目结构更改 一些数据库操作抽象 引入SQLite数据库后端支持

--- src/Main.cs
+++ src/Main.cs
@@ -7,14 +7,16 @@ namespace TGZG.战雷革命房间服务器 {
     public static partial class 公共空间 {
         public static string 版本 => "0.0.4";
         public static List<房间数据类> 房间列表 = new();
-        public static 自由空域数据库 数据库 = new();
+        public static 自由空域数据库 数据库 = new 自由空域数据库_SQLite();		//开发用SQLite数据库后端。
         public static Dictionary<SocketClient, string> 在线玩家 = new();
         public static void 启动() {
             TGZG.战雷革命房间服务器.房间管理信道.启动();
             TGZG.战雷革命房间服务器.玩家客户端管理信道.启动();
-            while (true) {
-                string 指令 = Console.ReadLine();
-            }
+			//以后还要做一个命令系统！
+			//这里暂时只挂一个循环转让处理器控制权给其它线程/进程的程序。
+			for (; ; ) {
+				System.Threading.Thread.Yield();
+			}
         }
     }
 }
---
+++ src/数据结构/玩家档案.cs
@@ -0,0 +1,22 @@
+﻿using System;//Action
+using System.ComponentModel.DataAnnotations;
+
+namespace TGZG.战雷革命房间服务器 {
+	public class 玩家档案 {
+		//TKTek: 这个是句柄吗？
+        [Key]
+        public int _ { get; set; }
+        public string 账号名 { get; set; }
+        public string 密码 { get; set; }
+
+        public int 击杀数 { get; set; }
+        public int 死亡数 { get; set; }
+        public int 助攻数 { get; set; }
+        public long 爬高总高度 { get; set; }
+        public long 能量转化总量 { get; set; }
+        public TimeSpan 语音总时长 { get; set; }
+        public long 消息发送总数 { get; set; }
+        public long 射出子弹总数 { get; set; }
+        public long 子弹命中次数 { get; set; }
+    }
+}
